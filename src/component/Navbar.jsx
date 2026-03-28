cf38aa2a921235f7f647a27f38ce44c06132ac2c
Priyanshu <priyanshukamboj1903@gmail.com>
2026-03-28 11:37:17 +0530

    Sixth Commit

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,12 +1,29 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faMagnifyingGlass, faClock } from '@fortawesome/free-solid-svg-icons'
 import { useNavigate } from 'react-router-dom'
+import { UserContextCreater } from '../context/UserContext'
 
 const Navbar = () => {
+  const [input,setInput]=useState();
+   const {data,setData}=useContext(UserContextCreater);
+  const [crdata,setCryData]=useState(data);
+ 
     const navigate=useNavigate()
     const handleClick=(e)=>{
         navigate("/home")
+    }
+    const handleSubmit=async(e)=>{
+      e.preventDefault();
+      const filter=data.filter((e,i)=>e.name.toLowerCase().includes(input.toLowerCase()));
+      setData(filter);
+    }
+
+    const handleChange=async(e)=>{
+      setInput(e.target.value);
+      if(e.target.value.trim()===""){
+        setData(crdata);
+      }
     }
   return (
     <div className='Navbar max-w-[1350px] flex justify-between mx-auto py-4 px-6 items-center text-white'>
@@ -22,10 +39,15 @@ const Navbar = () => {
         <form action="" className='flex items-center justify-between w-full'>
           <input
             type="text"
-            placeholder='Search Crypto'
+            placeholder='Search Crypto ...'
             className='w-full outline-none bg-transparent text-slate-300 placeholder:text-slate-500 text-sm'
+            list='coinList'
+            onChange={handleChange}
           />
-          <button type="submit" className='ml-3 cursor-pointer bg-cyan-400 text-[#0b0f1a] w-8 h-8 rounded-full flex items-center justify-center hover:bg-cyan-300 transition-colors duration-200'>
+          <datalist id='coinList' >
+              {data.map((e,i)=>(<option value={e.name}></option>))}
+          </datalist>
+          <button type="submit" className='ml-3 cursor-pointer bg-cyan-400 text-[#0b0f1a] w-8 h-8 rounded-full flex items-center justify-center hover:bg-cyan-300 transition-colors duration-200' onClick={handleSubmit}>
             <FontAwesomeIcon icon={faMagnifyingGlass} className="text-sm" />
           </button>
         </form>
